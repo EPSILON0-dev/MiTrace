2794b3f2c80039c7d52be06af92355192842a32d
epsiii <lforenc@wp.pl>
2026-02-07 21:10:46 +0100

    Fix camera aspect ratio

--- src/Trace/Trace.cpp
+++ src/Trace/Trace.cpp
@@ -18,8 +18,8 @@
 Ray Trace::GenerateCameraRay(float u, float v, float aspectRatio) const noexcept
 {
     float fovScale = tanf(camera_.GetFov() * 0.5f);
-    float px = (2.0f * u - 1.0f) * fovScale * aspectRatio;
-    float py = (2.0f * v - 1.0f) * fovScale;
+    float px = (2.0f * u - 1.0f) * fovScale;
+    float py = (2.0f * v - 1.0f) * fovScale / aspectRatio;
 
     glm::vec4 rayOriginCameraSpace = glm::vec4(0.0f, 0.0f, 0.0f, 1.0f);
     glm::vec4 rayDirectionCameraSpace = glm::normalize(glm::vec4(px, -py, -1.0f, 0.0f));
